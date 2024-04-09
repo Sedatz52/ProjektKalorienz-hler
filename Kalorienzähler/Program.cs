d0f52dd097b28899e0b909f9194e11a705f351dc
Talha Kilinc <talha.kilinc935@gmail.com>
2024-04-09 12:34:11 +0200

    update

--- Kalorienzähler/Program.cs
+++ Kalorienzähler/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Kalorienzähler.Daten;
+using System;
+
+namespace Kalorienzähler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var db = new KalorienzählerContext())
+            {
+                // Füge Produkte hinzu
+                var dolma = new Model.Produkt { Name = "Dolma", Kalorien = 150, Gramm = 100 };
+                var sarma = new Model.Produkt { Name = "Sarma", Kalorien = 180, Gramm = 100 };
+                var lahmacun = new Model.Produkt { Name = "Lahmacun", Kalorien = 300, Gramm = 100 };
+                var iclikofte = new Model.Produkt { Name = "Içli Köfte", Kalorien = 250, Gramm = 100 };
+
+                db.Produkte.AddRange(dolma, sarma, lahmacun, iclikofte);
+                db.SaveChanges();
+
+                // Füge einen Verbrauch hinzu
+                var verbrauch = new Model.Verbrauch { Zeitpunkt = DateTime.Now, Menge = 200 };
+                db.Verbrauche.Add(verbrauch);
+                db.SaveChanges();
+            }
+        }
+    }
+}
+
